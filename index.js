60a21c604c4dabf0033f6277af425b380651abfa
Chris Bonet <christopher.bonet@gmail.com>
2022-11-10 12:34:45 -0500

    commit tree 9f8638a755003f4ac1aec5d3e03d0f2d3727b6e0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const string = "birthday";
+
+function writeCards(cards, string) {
+    let newArray = [];
+    for (let i = 0; i < cards.length; i++) {
+    newArray.push(`Thank you, ${cards[i]}, for the wonderful ${string} gift!`);
+    debugger;
+    }
+
+return newArray;
+}
+
+writeCards(cards, string);
+
+function countDown() {
+    let countDown = 0;
+    while (countDown < 11) {
+        console.log(countDown++);
+    }
+
+return countDown;
+}
